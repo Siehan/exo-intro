1e3466a8b11a31126d70f9a634ea42c4c5e0056f
Siehan <memain.ye.sylvie@gmail.com>
2021-02-13 10:33:23 +0100

    with the manual counter & more comments ;-)

--- manual_count.js
+++ manual_count.js
@@ -1,6 +1,6 @@
 /*
-Créer un script manual_count.js
-En ne vous servant que de console.log écrivez un programme qui affiche les nombres de 0 à 20.
+Créez un script manual_count.js
+En ne vous servant que de console.log, écrivez un programme qui affiche les nombres de 0 à 20
 
 L'affichage à l'exécution du script devra ressembler à ça :
 
@@ -15,10 +15,38 @@ L'affichage à l'exécution du script devra ressembler à ça :
 20
 */
 
-for (let i = 0; i <= 20; ++i) {
-  console.log(i);
-}
+//manual counter
+
+console.log("0");
+console.log("1");
+console.log("2");
+console.log("3");
+console.log("4");
+console.log("5");
+console.log("6");
+console.log("7");
+console.log("8");
+console.log("9");
+console.log("10");
+console.log("11");
+console.log("12");
+console.log("13");
+console.log("14");
+console.log("15");
+console.log("16");
+console.log("17");
+console.log("18");
+console.log("19");
+console.log("20");
+
+//a counter with a loop
 
 for (let counter = 0; counter <= 20; counter += 1) {
   console.log(counter);
+}
+
+//another counter with a loop
+
+for (let i = 0; i <= 20; ++i) {
+  console.log(i);
 }
